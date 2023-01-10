b2b0aac82b11e7cdcb50d2939a2f31d2a489100f
caseyfee <caseyfee3@gmail.com>
2023-01-10 11:06:18 -0800

    Creation of Prompt messages

--- Develop/script.js
+++ Develop/script.js
@@ -14,13 +14,32 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Prompt for Password Requirements
-function myFunction() {
-  let text;
-  let person = prompt("Please choose what you'd like in a password:", "Harry Potter");
-  if (person == null || person == "") {
-    text = "User cancelled the prompt.";
+
+function myPromptsFunction() {
+  var hasCharacters = confirm('Click OK to confirm including lowercase, uppercase, numberic, and special characters in your password.');
+  var passwordLength = confirm('Click OK to confirm password will be between 8 and 128 characters in your password.');
+  // If false was chosen for hasCharaters, passwordLength, or Validation, then present user with a message
+
+  if (hasCharacters) {
+    console.log("Okay good-your password will use a variety of characters");
   } else {
-    text = "Hello " + person + "! How are you today?";
+    console.log(confirm("Your password will be much less secure without a variety of charaters! You may chose to start the generator over"));
   }
-  document.getElementById("demo").innerHTML = text;
+
+  if (passwordLength) {
+    console.log("Okay good-your password will be a decent length");
+  } else {
+    console.log(confirm("Your password will be much less secure if it less than 8 characters! You may chose to start the generator over"));
+  }
+
+  var validation = confirm('Your selections have been validated and your password will be generated shortly.');
 }
+
+  // Password Generator
+    // if characters was confirmed (was true), include in password creation
+    // if password length was confirmed (true) include in password creation
+
+  
+
+
+
