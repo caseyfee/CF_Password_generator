e9b08adb92b896ef8cc9c89cc730a270c86c49d9
caseyfee <caseyfee3@gmail.com>
2023-01-09 21:31:42 -0800

    testing prompt

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompt for Password Requirements
+function myFunction() {
+  let text;
+  let person = prompt("Please choose what you'd like in a password:", "Harry Potter");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person + "! How are you today?";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
