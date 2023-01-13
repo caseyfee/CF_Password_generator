18dc27f15910aab00f96f5089fd04b755612a985
caseyfee <caseyfee3@gmail.com>
2023-01-13 12:32:26 -0800

    re-ordered promt()

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,7 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
