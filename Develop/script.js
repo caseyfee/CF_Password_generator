b87e5118d1152fac07022217005bcc6a53cf39e2
caseyfee <caseyfee3@gmail.com>
2023-01-15 10:50:37 -0800

    compiled code

--- Develop/script.js
+++ Develop/script.js
@@ -23,88 +23,78 @@ function writePassword() {
 }
 
 function generatePassword() {
-
-  // Prompts for Password Requirements
-  // var passwordLength = passwordLength.length
-
-
-  // Length of Password
-  function myPromptsFunction() {
-    passwordLength = parseInt(prompt('How long do you want your password? [Between 8 and 128 characters]'));
-    if (passwordLength >= 8 & passwordLength <= 128 & true) //& (typeof(passwordLength)=Number)
-    {
-      confirm('Your selections have been validated and your password will be generated shortly.');
-      console.log("Okay good-your password will be a decent length");
-    } else (isNaN(passwordLength)); {
-      console.log(alert("Please enter a number between 8 and 128!"));
-      console.log(confirm("Just follow the directions."));
-    }
-
-    for (var i = 0; i < passwordLength.length; i++) {
-      if (hasNumCharacters) {//Add requirement that would push the password out of the loop if  passwordLength.length is met) {
-        password += hasNumCharacters[Math.floor(Math.random() * passwordLength.length)];
-      }
-      else {
-        console.log("weak password");
-      }
-
-      // Lowercase
-      var lowerCase = confirm('Click OK to confirm including lowercase characters in your password.')
-      if (lowerCase)
-      {
-        promptAnswers = promptAnswers.concat(lowerCharacterOptions); 
-        password += lowerCase[Math.floor(Math.random() * passwordLength.length)];
-      }
-      else {
-        console.log("weak password");
-      }
-      //   console.log("Okay good-Your password will use lowercase characters");
-      // } else {
-      //   console.log(confirm("Your password will be much less secure without a variety of characters! You may want to chose to start the generator over"));
-      // }
-     
-
-      // Upper Case
-      var upperCase = confirm('Click OK to confirm including uppercase characters in your password.')
-      if (upperCase)
-      {
-        promptAnswers = promptAnswers.concat(upperCharacterOptions);
-        password += upperCase[Math.floor(Math.random() * passwordLength.length)];
-      }
-      // console.log("Okay good-Your password will use upper characters");
-      // } else {
-      //   console.log(confirm("Your password will be much less secure without a variety of characters! You may want to chose to start the generator over"));
-      // }
-    
-      else {
-        console.log("weak password");
-       }
-
-
-      // Numbers
-      var num = confirm('Click OK to confirm including numberic characters in your password.')
-      if (num);
-      {
-        promptAnswers = promptAnswers.concat(numOptions);
-        password += numOptions[Math.floor(Math.random() * passwordLength.length)];
-      }
-      // if (hasNumCharacters) {
-      //   console.log("Okay good-Your password will use numberic and special characters");
-      // } else {
-      //   console.log(confirm("Your password will be much less secure without a variety of characters! You may want to chose to start the generator over"));
-      // }
-
-      // Special Characters
-      var special = confirm('Click OK to confirm including special characters in your password.')
-      if (special); {
-        promptAnswers = promptAnswers.concat(specialChar);
-        password += specialChar[Math.floor(Math.random() * passwordLength.length)];
-      }
-      return true;
-    }
+  password = "";
+  for (var = i; i<passwordLength; i++); {
+    var ranPassworkd = Math.floor(Math.random() * passwordLength);
+    password = password + promptAnswers[randomIndex];
   }
-  return password;
+  return password
+  // Prompts for Password Requirements
 }
+// var passwordLength = passwordLength.length
+
+
+
+function myPromptsFunction() {
+  promptAnswers = [];
+  // Length of Password
+  passwordLength = parseInt(prompt('How long do you want your password? [Between 8 and 128 characters]'));
+  if (passwordLength >= 8 & passwordLength <= 128 & true) //& (typeof(passwordLength)=Number)
+  {
+    confirm('Your selections have been validated and your password will be generated shortly.');
+    console.log("Okay good-your password will be a decent length");
+  } else (isNaN(passwordLength)); {
+    console.log(alert("Please enter a number between 8 and 128!"));
+    console.log(confirm("Just follow the directions."));
+  }
+
+  // Lowercase
+  var lowerCase = confirm('Click OK to confirm including lowercase characters in your password.')
+  if (lowerCase) {
+    promptAnswers = promptAnswers.concat(lowerCharacterOptions);
+    password += lowerCase[Math.floor(Math.random() * passwordLength.length)];
+    console.log("Okay good-Your password will use lowercase characters");
+  }
+  else {
+    console.log("weak password");
+  }
+
+  // Upper Case
+  var upperCase = confirm('Click OK to confirm including uppercase characters in your password.')
+  if (upperCase) {
+    promptAnswers = promptAnswers.concat(upperCharacterOptions);
+    password += upperCase[Math.floor(Math.random() * passwordLength.length)];
+  }
+
+  else {
+    console.log("weak password");
+  }
+
+
+  // Numbers
+  var num = confirm('Click OK to confirm including numberic characters in your password.')
+  for (var i = 0; i < passwordLength.length; i++) {
+    if (num) {//Add requirement that would push the password out of the loop if  passwordLength.length is met) {
+      promptAnswers = promptAnswers.concat(numOptions);
+      password += numOptions[Math.floor(Math.random() * passwordLength.length)];
+      password += hasNumCharacters[Math.floor(Math.random() * passwordLength.length)];
+      console.log("Okay good-Your password will use numberic and special characters");
+    }
+    else {
+      console.log("weak password");
+    }
+
+// Special Characters
+    var special = confirm('Click OK to confirm including special characters in your password.')
+    if (special); {
+      promptAnswers = promptAnswers.concat(specialChar);
+      password += specialChar[Math.floor(Math.random() * passwordLength.length)];
+    }
+    return true;
+  }
+}
+  // return password;
+
 
 
 
